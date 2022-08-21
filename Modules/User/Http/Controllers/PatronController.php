abfcc460e0a102a8bf76964ce09bbc39711dd264
mhmoudsh <mmh1999.2014@gmail.com>
2022-08-21 14:15:35 +0300

    mm

--- Modules/User/Http/Controllers/PatronController.php
+++ Modules/User/Http/Controllers/PatronController.php
@@ -3,13 +3,14 @@
 namespace Modules\User\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Modules\Places\Entities\Bank;
 use Modules\Places\Entities\City;
 use Modules\User\Entities\Patron;
 use Illuminate\Routing\Controller;
-use Illuminate\Contracts\Support\Renderable;
-use Modules\Places\Entities\Bank;
 use Modules\Places\Entities\Branch;
 use Modules\User\Entities\Customer;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Contracts\Support\Renderable;
 
 class PatronController extends Controller
 {
@@ -17,9 +18,46 @@ class PatronController extends Controller
      * Display a listing of the resource.
      * @return Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('user::index');
+         if ($request->ajax()) {
+
+                $data =Patron::select([
+                'id',
+                'name',
+                'identification_number',
+                ]);
+                return DataTables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                $btn = ' <a href="' . route('patron.edit', [$row->id]) . '" title="edit" class="dropdown-item"
+                    style="display: contents">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="feather feather-archive font-small-4 mr-50">
+                        <polyline points="21 8 21 21 3 21 3 8">
+                        </polyline>
+                        <rect x="1" y="3" width="22" height="5"></rect>
+                        <line x1="10" y1="12" x2="14" y2="12"></line>
+                    </svg></a>';
+
+                $btn = $btn .'<a href="javascript:void(0)" data-id="' . $row->id . '" title="delete" style="display: contents"
+                    class="dropdown-item deletecourt"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="feather feather-trash-2 font-small-4 mr-50">
+                        <polyline points="3 6 5 6 21 6"></polyline>
+                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                    </svg></a>';
+
+                return $btn;
+                })
+                ->rawColumns(['product','action'])
+                ->make(true);
+         }
+
+        return view('user::patron.view-details');
     }
 
     /**
--- Modules/User/Resources/views/patron/view-details.blade.php
+++ Modules/User/Resources/views/patron/view-details.blade.php
@@ -1,4 +1,179 @@
 @extends('base::master')
 @section('content')
+    <section class="app-user-list">
 
-@endsection
+                    <div class="dt-buttons btn-group flex-wrap ">
+                        <a href="{{ route('patron.create') }}" class="btn add-new btn-primary mt-50"
+                            style="margin: 20px;">Add product</a>
+                    </div>
+                    <!-- list section start -->
+                    <div class="card">
+
+                        <div class="container">
+
+                            <div class="card-datatable table-responsive pt-0">
+                                <table class="court-list-table table">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th></th>
+                                            <th>name</th>
+                                            <th>identification_number</th>
+                                            <th>action</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- list section end -->
+                </section>
+    @endsection
+            @section('js')
+                <script src="{{asset('datatable/jquery.js')}}"></script>
+                <script src="{{asset('datatable/jquery.dataTables.min.js')}}"></script>
+                <script src="{{asset('datatable/datatables.bootstrap4.min.js')}}"></script>
+                <script src="{{asset('datatable/dataTables.responsive.min.js')}}"></script>
+                <script src="{{asset('datatable/responsive.bootstrap4.js')}}"></script>
+                <script src="{{asset('datatable/datatables.checkboxes.min.js')}}"></script>
+                <script src="{{asset('datatable/datatables.buttons.min.js')}}"></script>
+                <script src="{{asset('datatable/jszip.min.js')}}"></script>
+                <script src="{{asset('datatable/pdfmake.min.js')}}"></script>
+                <script src="{{asset('datatable/vfs_fonts.js')}}"></script>
+                <script src="{{asset('datatable/buttons.html5.min.js')}}"></script>
+                <script src="{{asset("datatable/buttons.print.min.js")}}"></script>
+                <script src="{{asset("datatable/dataTables.rowGroup.min.js")}}"></script>
+                <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+                <script type="text/javascript">
+                    $(function () {
+                        $.ajaxSetup({
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                            }
+                        });
+
+                        // ################## Index #################################
+
+                        var table = $('.court-list-table').DataTable({
+                            processing: true,
+                            serverSide: true,
+                            ajax: "{{ route('patron.index') }}",
+                            /*  "language": {
+                                "url": "{{ asset('datatableLang/'.app()->getLocale() . '.json') }}"
+                            }, */
+                            columns: [{
+                                    data: 'id',
+                                    name: 'id'
+                                },
+                                {
+                                    data: 'name',
+                                    name: 'name'
+                                },
+                                {
+                                    data: 'identification_number',
+                                    name: 'identification_number'
+                                },
+                                {
+                                    data: 'action',
+                                    name: 'action',
+                                    orderable: false,
+                                    searchable: false
+                                },
+                            ],
+                            buttons: [{
+                                text: 'Add customer',
+                                className: 'add-new btn btn-primary mt-50',
+                                attr: {
+                                    'data-toggle': 'modal',
+                                    'data-target': '#modals-slide-in'
+                                },
+                                init: function (api, node, config) {
+                                    $(node).removeClass('btn-secondary');
+                                }
+                            }],
+                            responsive: {
+                                details: {
+                                    display: $.fn.dataTable.Responsive.display.modal({
+                                        header: function (row) {
+                                            var data = row.data();
+                                            return 'Details of ' + data['name'];
+                                        }
+                                    }),
+                                    type: 'column',
+                                    renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+                                        tableClass: 'table',
+                                        columnDefs: [{
+                                                targets: 2,
+                                                visible: false
+                                            },
+                                            {
+                                                targets: 3,
+                                                visible: false
+                                            }
+                                        ]
+                                    })
+                                }
+                            },
+
+                        });
+
+                        // ################## Delete ####################################
+
+                        $('body').on('click', '.deletecourt', function () {
+
+                        var court_id = $(this).data("id");
+                        Swal.fire({
+                            title: '{{ __('messages.are you sure?') }}',
+                            text: "{{ __('messages.you will not be able to revert this!') }}",
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: '{{ __('messages.yes, delete it!') }}'
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                            $.ajax({
+                                            data: {
+                                                        "_token": "{{ csrf_token() }}",
+                                                        "court_id": court_id
+                                                    },
+                                                type: "DELETE",
+                                                url: "{{ route('patron.store') }}" +
+                                                    '/' + court_id,
+                                                success: function (data) {
+                                                    table.draw();
+                                                },
+                                                error: function (data) {
+                                                    console.log('Error:', data);
+                                                }
+                                            });
+                                            Swal.fire(
+                                                'هل انت متاكد من الحذف',
+                                                'تم الحذف بنجاح',
+                                            'تم الحذف بنجاح',
+                                            )
+                                }
+                        });
+                    });
+
+
+                    });
+
+                    </script>
+
+                <script>
+                    $(window).on('load', function () {
+                        if (feather) {
+                            feather.replace({
+                                width: 14,
+                                height: 14
+                            });
+                        }
+                    })
+
+                </script>
+            @endsection
+
+
